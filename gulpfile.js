2078811ef47ac76e37c51291e39a58db422dd6b8
Dominic Magnifico <dominic@zenman.com>
2014-09-05 18:34:28 -0600

    Add shorthand vars, clean up name specific paths for theme and db

--- gulpfile.js
+++ gulpfile.js
@@ -5,17 +5,17 @@
 /*------------------------------------*\
     ::Plugins
 \*------------------------------------*/
-var gulp = require('gulp');
-var livereload = require('gulp-livereload');
-var uglify = require('gulp-uglifyjs');
-var jshint = require('gulp-jshint');
-var compass = require('gulp-compass');
-var prefix = require('gulp-autoprefixer');
-var iconfont = require('gulp-iconfont');
-var iconfontCss = require('gulp-iconfont-css');
-var svgSprites = require('gulp-svg-sprites');
-var shell = require('gulp-shell')
-var exit = require('gulp-exit');
+var gulp = require('gulp'),
+livereload = require('gulp-livereload'),
+uglify = require('gulp-uglifyjs'),
+jshint = require('gulp-jshint'),
+compass = require('gulp-compass'),
+prefix = require('gulp-autoprefixer'),
+iconfont = require('gulp-iconfont'),
+iconfontCss = require('gulp-iconfont-css'),
+svgSprites = require('gulp-svg-sprites'),
+shell = require('gulp-shell'),
+exit = require('gulp-exit');
 
 /*------------------------------------*\
     ::Handle Errors
@@ -31,36 +31,36 @@ function handleError(err) {
 
 //js
 gulp.task('js', function() {
-    gulp.src('wp-content/themes/zemplate/js/src/**/*.js')
+    gulp.src('wp-content/themes/__MYTHEMEHERE__/js/src/**/*.js')
         .pipe(jshint())
         .pipe(jshint.reporter('default'))
         .pipe(uglify('scripts.min.js', {
             outSourceMap: 'src/sourcemap.map',
-            basePath: '/wp-content/themes/zemplate/js/src/'
+            basePath: '/wp-content/themes/__MYTHEMEHERE__/js/src/'
         }))
-        .pipe(gulp.dest('wp-content/themes/zemplate/js/'));
+        .pipe(gulp.dest('wp-content/themes/__MYTHEMEHERE__/js/'));
 });
 
 //css
 gulp.task('css', function() {
-    gulp.src('wp-content/themes/zemplate/sass/*.scss')
+    gulp.src('wp-content/themes/__MYTHEMEHERE__/sass/*.scss')
         .pipe(compass({
             sourcemap: true,
             quiet: true,
-            css: 'wp-content/themes/zemplate/',
-            sass: 'wp-content/themes/zemplate/sass',
-            image: 'wp-content/themes/zemplate/images',
+            css: 'wp-content/themes/__MYTHEMEHERE__/',
+            sass: 'wp-content/themes/__MYTHEMEHERE__/sass',
+            image: 'wp-content/themes/__MYTHEMEHERE__/images',
             style: 'compressed',
             require: ['sass-globbing']
         }))
         .on("error", handleError)
         .pipe(prefix('last 2 version', 'ie 10', 'ie 9'))
-        .pipe(gulp.dest('wp-content/themes/zemplate/.'));
+        .pipe(gulp.dest('wp-content/themes/__MYTHEMEHERE__/.'));
 });
 
 //webfonts
 gulp.task('icons', function(){
-    gulp.src(['wp-content/themes/zemplate/fonts/icons-raw/*.svg'])
+    gulp.src(['wp-content/themes/__MYTHEMEHERE__/fonts/icons-raw/*.svg'])
         .pipe(iconfontCss({
             path: 'icons-template.scss-template',
             fontName: 'icon',
@@ -70,25 +70,25 @@ gulp.task('icons', function(){
         .pipe(iconfont({
             fontName: 'icon'
         }))
-        .pipe(gulp.dest('wp-content/themes/zemplate/fonts/icons/'));
+        .pipe(gulp.dest('wp-content/themes/__MYTHEMEHERE__/fonts/icons/'));
 });
 
 //svg sprites
 var svg = svgSprites.svg;
 gulp.task('sprite', function () {
-    gulp.src('wp-content/themes/zemplate/images/svg-raw/*.svg')
+    gulp.src('wp-content/themes/__MYTHEMEHERE__/images/svg-raw/*.svg')
         .pipe(svg({
             defs: true,
             generatePreview: false
         }))
-        .pipe(gulp.dest("wp-content/themes/zemplate/images/svg-sprites"));
+        .pipe(gulp.dest("wp-content/themes/__MYTHEMEHERE__/images/svg-sprites"));
 });
 
 //database
 var config = {
     db: {
         local: {
-            name: 'l1_whv',
+            name: 'l1_00000000000000',
             user: 'root',
             pass: 'root',
             host: 'localhost',
@@ -126,13 +126,13 @@ var server = livereload();
 gulp.task('watch', function() {
 
     //run tasks when watch notices changes
-    gulp.watch('wp-content/themes/zemplate/sass/**/*.scss', ['css']);
-    gulp.watch('wp-content/themes/zemplate/js/src/**/*.js', ['js']);
-    gulp.watch('wp-content/themes/zemplate/fonts/icons-raw/*.svg', ['icons']);
-    gulp.watch('wp-content/themes/zemplate/images/svg-raw/*.svg', ['sprite']);
+    gulp.watch('wp-content/themes/__MYTHEMEHERE__/sass/**/*.scss', ['css']);
+    gulp.watch('wp-content/themes/__MYTHEMEHERE__/js/src/**/*.js', ['js']);
+    gulp.watch('wp-content/themes/__MYTHEMEHERE__/fonts/icons-raw/*.svg', ['icons']);
+    gulp.watch('wp-content/themes/__MYTHEMEHERE__/images/svg-raw/*.svg', ['sprite']);
 
     //reload the project if certain files change
-    gulp.watch('wp-content/themes/zemplate/**/*.{css,html,php,js,svg}').on('change', function(file) {
+    gulp.watch('wp-content/themes/__MYTHEMEHERE__/**/*.{css,html,php,js,svg}').on('change', function(file) {
         server.changed(file.path);
     });
 });
