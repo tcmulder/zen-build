641ac000579798287678622642ae28916bc514b3
tcmulder <tcmulder@gmail.com>
2015-03-21 16:45:05 -0600

    Update the config to export an object

--- zen-config.js
+++ zen-config.js
@@ -1,6 +1,4 @@
-// Explain the whole thing bigly
-module.exports = {
-    // explain what this one is...
+var config = {
     site: {
         client: 'CLIENTNAME',
         proj:   'PROJECTNAME'
@@ -42,14 +40,5 @@ module.exports = {
             dumpFile: 'db.sql'
         }
     }
-    //,
-    // this gulpfile already handles from/to,
-    // otherwise you'd put those here. If you
-    // do add strings here, they will overwrite
-    // those auto-generated from the gulpfile.
-    // url: {
-    //     src: '',
-    //     dest: ''
-    // }
-
-};
+}
+module.exports = config;
